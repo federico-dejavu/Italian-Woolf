85c69e6509dfcf1f3c6f220c480a2fda8e43c380
Federico Fogli <federico@dejavu.it>
2020-07-12 19:59:00 +0200

    meno dump

--- ajax/searchKeywords.php
+++ ajax/searchKeywords.php
@@ -26,7 +26,6 @@ ini_set('display_errors',1); # uncomment if you need debugging
         $arrayWorks=array();
         foreach($allWorksID as $work_id){
             $singleWork = $works->getWorksByWork_id($work_id);
-            var_dump($singleWork);
             $publisherObject = new publishers();
             $publisher = $publisherObject->getPublisherById($singleWork['publisher_id']);
             var_dump($publisher);
