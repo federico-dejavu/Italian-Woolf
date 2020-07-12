1d11e15f0836d0cdfc5c85e142fba77da8316cc1
Ghizzoni <mariza.ghizzoni@mail-bip.com>
2020-07-12 19:23:11 +0200

    utf8 encode

--- ajax/searchKeywords.php
+++ ajax/searchKeywords.php
@@ -23,6 +23,7 @@ ini_set('display_errors',1); # uncomment if you need debugging
         $allWorksID = $works->getWorkIdByKeywords($keyOptimized);
         //var_dump($allWorksID);
         $arrayWorks = $works->getWorksByListOfWork_id($allWorksID);
+        $arrayWorks = array_map(utf8_encode($arrayWorks));
 
       
         
