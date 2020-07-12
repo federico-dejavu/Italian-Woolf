f37b430c59ef1faf7a1269b82e1bd99de1245304
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-07-12 15:14:45 +0200

    Create base-config.php

---
+++ include/base-config.php
@@ -0,0 +1,11 @@
+<?php
+
+	define('WOOLF_DB_SERVER', "127.0.0.1");
+	define('WOOLF_DB_USER', "");
+	define('WOOLF_DB_PASSWORD', "");
+	define('WOOLF_DB_NAME', "");
+
+	define('WOOLF_PATH', dirname(dirname(__FILE__)));
+	define('WOOLF_URL', '');
+
+?>
