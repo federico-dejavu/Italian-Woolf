275b7d04b2b5332809ae01fb369cadc758a8a408
Ghizzoni <mariza.ghizzoni@mail-bip.com>
2020-07-18 15:50:11 +0200

    people_id

--- include/authors.php
+++ include/authors.php
@@ -12,11 +12,11 @@ class authors{
         if($id>0){
             $db = new DBManager();
             $query = "SELECT peoples_id FROM works_authors WHERE works_id = $id";
-            $arrPeoples = $db->query($query);
+            $arrPeoples[] = $db->query($query);
             $people = new peoples();
             foreach($arrPeoples as $key=>$value){
                 var_dump($value);
-                $author = $people->getPeopleById($value['id']);
+                $author = $people->getPeopleById($value['people_id']);
                 $arrElements = $author;
             }
         }
