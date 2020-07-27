fe278cc60c3e6189c50db0aeaa206a9dd695acb4
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-07-27 21:52:05 +0200

    Javascript out of on load

--- js/ajax.js
+++ js/ajax.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
 		
-	var baseUrl = 'https://italianwoolf.reading.ac.uk/stage/';
-
-	function search(action) {
-
-		$.ajax({
-			type: "POST",
-			url: baseUrl + 'ajax/' + action + '.php',
-			async: false,
-			data: $("#search").serialize(),
-			success: function(html) {
-				$('#result').html(html);;
-			}
-
-		})		
-
-	}
 
 });
+
+
+function ajaxSearch(action) {
+
+	var baseUrl = 'https://italianwoolf.reading.ac.uk/stage/';
+
+	$.ajax({
+		type: "POST",
+		url: baseUrl + 'ajax/' + action + '.php',
+		async: false,
+		data: $("#search").serialize(),
+		success: function(html) {
+			$('#result').html(html);;
+		}
+
+	})		
+
+}
