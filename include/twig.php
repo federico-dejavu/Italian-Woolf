f08b7d5ba279b48a4fce967f0f5e0ccaae2ffadb
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-08-28 14:22:09 +0200

    Twig fix 11

--- include/twig.php
+++ include/twig.php
@@ -1,18 +1,32 @@
 <?php
 	
-	spl_autoload_register(function ($classname) {
-        $dirs = array (
-            '/include/Twig/' #./path/to/dir_where_src_renamed_to_Twig_is_in
-        );
+    spl_autoload_register(function ($class) {
 
-        foreach ($dirs as $dir) {
-            $filename = $dir . str_replace('\\', '/', $classname) .'.php';
-            if (file_exists($filename)) {
-                require_once $filename;
-                break;
-            }
+        // project-specific namespace prefix
+        $prefix = 'Twig';
+    
+        // base directory for the namespace prefix
+        $base_dir = __DIR__ . '/Twig/';
+    
+        // does the class use the namespace prefix?
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // no, move to the next registered autoloader
+            return;
         }
-
+    
+        // get the relative class name
+        $relative_class = substr($class, $len);
+    
+        // replace the namespace prefix with the base directory, replace namespace
+        // separators with directory separators in the relative class name, append
+        // with .php
+        $file = $base_dir . str_replace('_', '/', $relative_class) . '.php';
+    
+        // if the file exists, require it
+        if (file_exists($file)) {
+            require $file;
+        }
     });
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
