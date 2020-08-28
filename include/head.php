01446901d617c5f397b8491f6539d04fa2e946d3
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-08-28 11:09:06 +0200

    Setting up template 0.2

---
+++ include/head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="it"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>VIRGINIA WOOLF IN ITALY</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<meta name="viewport" content="width=device-width">
+
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet/less" type="text/css" href="css/style.less" />
+    
+
+	<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+	<script src="//cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="js/less.js"></script>
+	<script type="text/javascript" src="js/ajax.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	<link rel="stylesheet" href="https://use.typekit.net/ffn1kbh.css">
+</head>
