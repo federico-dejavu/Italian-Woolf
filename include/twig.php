317d3ffb1ce589b5fe30cc9a78b2548fe45d049a
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-08-28 14:14:21 +0200

    Twig fix 7

--- include/twig.php
+++ include/twig.php
@@ -1,19 +1,19 @@
 <?php
 	
 	spl_autoload_register(function ($classname) {
-    $dirs = array (
-        '/include/Twig/' #./path/to/dir_where_src_renamed_to_Twig_is_in
-    );
+        $dirs = array (
+            './include/Twig/' #./path/to/dir_where_src_renamed_to_Twig_is_in
+        );
 
-    foreach ($dirs as $dir) {
-        $filename = $dir . str_replace('\\', '/', $classname) .'.php';
-        if (file_exists($filename)) {
-            require_once $filename;
-            break;
+        foreach ($dirs as $dir) {
+            $filename = $dir . str_replace('\\', '/', $classname) .'.php';
+            if (file_exists($filename)) {
+                require_once $filename;
+                break;
+            }
         }
-    }
 
-});
+    });
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
