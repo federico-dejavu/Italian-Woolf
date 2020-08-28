a25743136cf6d734e3bf25672dd196c05f04f808
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-08-28 14:03:06 +0200

    Twig fix 6

--- include/twig.php
+++ include/twig.php
@@ -2,7 +2,7 @@
 	
 	spl_autoload_register(function ($classname) {
     $dirs = array (
-        '../include/Twig/' #./path/to/dir_where_src_renamed_to_Twig_is_in
+        'Twig/' #./path/to/dir_where_src_renamed_to_Twig_is_in
     );
 
     foreach ($dirs as $dir) {
