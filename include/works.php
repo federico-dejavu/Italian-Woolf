c35b5cea756c2826ed0aae4c5077a07d9d58dadc
Ghizzoni <mariza.ghizzoni@mail-bip.com>
2020-08-29 22:05:14 +0200

    tolto dump

--- include/works.php
+++ include/works.php
@@ -114,11 +114,6 @@ class works{
         }   
 
         $query = $query.$from." WHERE ".$where." order by works.title asc";
-
-        
-    echo "<pre> Query works</br>";
-    var_dump($query);
-    echo "</pre>"; 
          
         $arrWorksID = $db->queryList($query);
         return $arrWorksID;
