f52ef3f566ffdbb4867cc1f4da2b30c8b8009183
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-12-07 10:42:19 +0100

    fix templates dir

--- advancedSearch.php
+++ advancedSearch.php
@@ -6,4 +6,4 @@
 	include_once('include/head.php');
 	include_once('include/header.php');
 
-    echo $twig->render('result/advancesSearch.html');
+    echo $twig->render('templates/advancesSearch.html');
