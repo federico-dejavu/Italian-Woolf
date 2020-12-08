ddafc3e1e1f4e352d7a541f0ae8093a7f2225096
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2020-12-08 16:11:15 +0100

    publishers

--- advancedSearch.php
+++ advancedSearch.php
@@ -5,5 +5,14 @@
 	
 	include_once('include/head.php');
 	include_once('include/header.php');
+	include_once('include/publishers.php');
 
-    echo $twig->render('advancedSearch.html');
+	$publishers_obj = new publishers();
+	$publishers = $publishers_obj->getAllPublishers();
+
+    echo $twig->render('advancedSearch.html', [
+        'publishers' => $publishers,
+	]);
+
+
+?>
