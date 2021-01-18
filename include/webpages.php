c4751704ffbba0fb4e72c8f497e843922deec7e2
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-01-18 15:28:43 +0100

    Webpage 0.0.1.5

--- include/webpages.php
+++ include/webpages.php
@@ -11,9 +11,10 @@ class webpages{
             $query = "SELECT `id`, `title`, `menu_title`, `content`, `content_key`, `parent_id`, `languages_id` FROM `pages` WHERE id = $id";
             $Webpage = $db->queryList($query);
 		}
-		var_dump($Webpage);
+
         return $Webpage;
-    }    
+
+	}    
 	
 	/* Dato un content_key e un language_id restituisce un array di del contenuto della pagina */
 	public function getWebpageByContentKeyId($content_key = "", $languages_id = "1") {
@@ -23,7 +24,10 @@ class webpages{
             $query = "SELECT `id`, `title`, `menu_title`, `content`, `content_key`, `parent_id`, `languages_id` FROM `pages` WHERE content_key = '$content_key' AND languages_id = $languages_id";
             $Webpage = $db->queryList($query);
         }
-        return $Webpage;
+
+		var_dump($Webpage);
+		return $Webpage;
+		
     }    
 
 }
