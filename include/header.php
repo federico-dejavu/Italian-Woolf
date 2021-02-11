964db1b05ece0246955747f764f4e9a835796f68
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-02-11 12:33:14 +0100

    Web 0.7.2

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
 	<div id="header">
 	    <div class="container">
 			<div class="row align-items-start">
-				<div class="col-sm-2">
+				<div class="col offset-1">
 					<a class="title" href="<?php echo WOOLF_URL; ?>">
 						VIRGINIA
 						<br />
