2023020d161a35fff73e714e047f6e0c6bd6a679
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-03-11 09:25:10 +0100

    Stage 0.2.1

--- advancedSearch.php
+++ advancedSearch.php
@@ -1,38 +1,6 @@
-<?php
+<?php 
+	include_once('include/webpages.php'); 
 
-	require_once 'include/config.php';
-    require_once 'include/cleaner.php';
-	
-	include_once('include/publishers.php');
-	include_once('include/languages.php');
-	include_once('include/typologies.php');
-
-	$publishers_obj = new publishers();
-	$publishersList = $publishers_obj->getAllPublishers();
-	foreach($publishersList as $publisherID){
-		$publisherSingle = $publishers_obj->getPublisherById($publisherID);
-		$publishers[$publisherID] = $publisherSingle;
-	}
-
-	$languages_obj = new languages();
-	$languagesList = $languages_obj->getAllLanguages();
-	foreach($languagesList as $languageID){
-		$languageSingle = $languages_obj->getLanguageById($languageID);
-		$languages[$languageID] = $languageSingle;
-	}
-
-	$typologies_obj = new typologies();
-	$typologiesList = $typologies_obj->getAllTypologies();
-	foreach($typologiesList as $typologyID){
-		$typologySingle = $typologies_obj->getTypologyById($typologyID);
-		$typologies[$typologyID] = $typologySingle;
-	}
-
-
-	echo $twig->render('advancedSearch.html', [
-        'publishers'	=> $publishers,
-		'languages'		=> $languages,
-		'typologies'	=> $typologies,
-	]);
+	renderPage('ADVSEARCH');
 
 ?>
