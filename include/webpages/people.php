9c8789634b146183826320d3afd12ac94eecbcdb
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-03-11 13:26:48 +0100

    Stage 0.4.5.10

--- include/webpages/people.php
+++ include/webpages/people.php
@@ -3,6 +3,7 @@
 	require_once 'include/config.php';
     require_once 'include/peoples.php';
     require_once 'include/authors.php';
+    require_once 'include/works.php';
 
     $id	= (isset($_REQUEST['id'])	? $_REQUEST['id']	: '');
 
@@ -13,11 +14,16 @@
     /* Reperisco dati works come author */
     $authorObject = new authors();
     $worksByAuthorId = $authorObject->getWorksByAuthorId($id);
-    
-    var_dump($worksByAuthorId);
-    
+    $authorWorksObject = new works();
+    $authorWorksAll = array();
+    foreach ($worksByAuthorId as $authorWork_id ) {
 
+        $authorWorks = $authorWorksObject->getWorksByWork_id($authorWork_id);
 
+        $authorWorksAll[] = $authorWorks;        
+    }
+
+    var_dump($authorWorksAll);
 /**  
    $people[]
         id
