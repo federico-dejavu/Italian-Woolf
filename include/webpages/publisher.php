8ea54cd1bbf72cf500a99547e5f185dc36361720
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-03-11 10:43:09 +0100

    Stage 0.4.5.1

--- include/webpages/publisher.php
+++ include/webpages/publisher.php
@@ -5,12 +5,11 @@
 
     $id	= (isset($_REQUEST['id'])	? $_REQUEST['id']	: '');
 
-    if($id > 0){
-        /* Reperisco dati publisher */
-        $publisherObject = new publishers();
-        $publisher = $publisherObject->getPublisherById($id);
+    /* Reperisco dati publisher */
+    $publisherObject = new publishers();
+    $publisher = $publisherObject->getPublisherById($id);
 
-/**
+    /**
     publisher
         id, 
         publisher, 
