db668156bc89c6aa311e5fcf068f638fe6121360
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-03-11 09:40:14 +0100

    Stage 0.2.4.1

--- include/webpages.php
+++ include/webpages.php
@@ -110,7 +110,6 @@ function renderPage($content_key = "HOME") {
     ["parent_id"]
     ["languages_id"]
 */
-    var_dump($page);
 
     $file_name = WOOLF_PATH.'/css/'.$page['file_name'].'.less';
     if (!file_exists($filename)) {
