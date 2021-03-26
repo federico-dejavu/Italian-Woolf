0c99c92ee43a2ee259cedac47dcd6e07048176d8
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-03-26 21:20:20 +0100

    Page title 0.2

--- include/webpages.php
+++ include/webpages.php
@@ -93,7 +93,7 @@ function renderPage($content_key = "HOME") {
 
         $renderTarget = $page['file_name'].'.html';
         include_once('webpages/'.$page['file_name'].'.php');
-        if $phpPage['work_title'] != '' ? $page['title'] = $phpPage['work_title'];
+        $page['title'] = isset($phpPage['work_title']) ? $phpPage['work_title'] : $page['title'];
 
     }
 
