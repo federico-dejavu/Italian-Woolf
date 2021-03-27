5b297fe3461c3a0fcdc618d910f33c9185c4bfb9
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-03-27 16:17:22 +0100

    Translators 0.7

--- include/translators.php
+++ include/translators.php
@@ -24,7 +24,7 @@ class translators{
     
             if($id>0){
                 $db = new DBManager();
-                $query = "SELECT peoples_id FROM editions_translators WHERE works_id = $id";
+                $query = "SELECT peoples_id FROM editions_translators WHERE editions_id = $id";
                 $arrPeoples = $db->queryList($query);
             }
         return $arrPeoples;
