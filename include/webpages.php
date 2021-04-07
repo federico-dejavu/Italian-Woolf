6720c5286ebbfa25737d2ba47bca0d13f8832a4d
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-04-07 22:29:32 +0200

    Languages 0.1.3

--- include/webpages.php
+++ include/webpages.php
@@ -2,6 +2,7 @@
 
 require_once('include/config.php');
 require_once('DBManager.php');
+require_once('functions/functions.php');
 
 class webpages{
     
@@ -66,85 +67,5 @@ class webpages{
    }
 
 }
-
-function renderPage($content_key = "HOME") {
-   
-    $languages_id = checkLanguage();
-    $renderTarget = 'webpages.html';
-    $phpPage = array();
-
-    // Reperisco i contenuti ella webpage
-    $pageObject = new webpages();
-    $page = $pageObject->getWebpageByContentKeyId($content_key,$languages_id);
-    $args = $pageObject->renderPageArgs();
-
-    if ($page["type"] == "php") {
-
-        $renderTarget = $page['file_name'].'.html';
-        include_once('webpages/'.$page['file_name'].'.php');
-        switch($page['file_name']) {
-            case 'work';
-                $page['title'] = $phpPage['work']['title'];
-                break;
-            case 'edition';
-                $page['title'] = $phpPage['edition']['title'];
-                break;
-            case 'article';
-                $page['title'] = $phpPage['article']['title'];
-                break;
-            case 'people';
-                $page['title'] = $phpPage['people']['fullname'];
-                break;
-            case 'publisher';
-                $page['title'] = $phpPage['publisher']['publisher'];
-                break;
-        }
-
-    }
-
-/*
-    ["id"]
-    ["title"]
-    ["menu_title"]
-    ["file_name"]
-    ["hidden"]
-    ["type"]
-    ["content"]
-    ["content_key"]
-    ["parent_id"]
-    ["languages_id"]
-*/
-
-    $file_name = WOOLF_PATH.'/css/'.$page['file_name'].'.less';
-    if (!file_exists($filename)) {
-        $file_name = NULL;
-    }
-
-    $menuObject = new webpages();
-    $menu = $menuObject->getMenuByParentid($page["parent_id"],$languages_id);
- 
-    
-
-    $loader = new \Twig\Loader\FilesystemLoader('templates');
-    $twig = new \Twig\Environment($loader, [
-        'cache' => false,
-    ]);
- 
-    include_once('templates/lang/lang'.$languages_id.'.php');
-
-    echo $twig->render($renderTarget, [
-        'SITE_TITLE'	=> SITE_TITLE,
-        'WOOLF_URL'	    => WOOLF_URL,
-        'menus'         => $menu,
-        'page'			=> $page,
-        'args'          => $args,
-        'page_name' 	=> $page_name,
-        'file_name'     => $file_name,
-        'phpPage'       => $phpPage,
-        'LANG'          => $language,
-    ]);
-
-}
-
 
 ?>
