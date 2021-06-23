d0fe2e4b8ea9d57fedda262b280387688df961a5
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-06-23 22:49:25 +0200

    Search

--- search.php
+++ search.php
@@ -1,11 +1,6 @@
 <?php 
-	require_once 'include/config.php';
-	$page_name = preg_replace('/\\.[^.\\s]{3,4}$/', '',basename($_SERVER['PHP_SELF']));
-	include_once('include/head.php'); 	
-	include_once('include/header.php'); 
+	include_once('include/webpages.php'); 
 
-	echo $twig->render('search.html');
+	renderPage('ADVSEARCH');
 
-	include_once('include/footer.php'); 
-	
 ?>
