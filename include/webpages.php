60a93437c1b389c3190f8a716d204d148bdf92f0
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2021-06-23 23:25:33 +0200

    Menu order

--- include/webpages.php
+++ include/webpages.php
@@ -40,7 +40,6 @@ class webpages{
 
         $db = new DBManager();
         $queryList = "SELECT `id` FROM `pages` WHERE `parent_id` = '$parent_id' AND `languages_id` = '$languages_id' ORDER BY `priority`,`title` ASC";
-        echo $queryList;
         $menuList = $db->queryList($queryList);
         foreach ($menuList as $menu_id){
             $query = "SELECT `id`, `menu_title`,`file_name`, `hidden`,  `content_key`, `parent_id`, `languages_id` FROM `pages` WHERE `id` = '$menu_id'";
