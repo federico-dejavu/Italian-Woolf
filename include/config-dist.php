61d055876c6e80ad25c928d41ca0e84253c5b042
federico-dejavu <64024935+federico-dejavu@users.noreply.github.com>
2022-05-21 12:35:58 +0000

    Config Dist

---
+++ include/config-dist.php
@@ -0,0 +1,22 @@
+<?php
+
+	define('WOOLF_DB_SERVER', "127.0.0.1");
+	define('WOOLF_DB_USER', "");
+	define('WOOLF_DB_PASSWORD', "");
+	define('WOOLF_DB_NAME', "");
+
+	define('SITE_TITLE', "");
+
+	define('WOOLF_PATH', dirname(dirname(__FILE__)));
+	define('WOOLF_URL', '');
+	
+	define('DEBUG',FALSE);
+
+	require_once WOOLF_PATH.'/vendor/autoload.php';
+	
+	$loader = new \Twig\Loader\FilesystemLoader('templates');
+	$twig = new \Twig\Environment($loader, [
+	   'cache' => false,
+	   // 'cache' => WOOLF_PATH.'/cache',
+	]);
+?>
